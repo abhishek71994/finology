1d07ebf73dac6cb028c9f3627e2645e0216362ae
abhishek.prasad <abhishek71994@gmail.com>
2022-06-30 13:32:46 +0800

    Adding basic specs for controller

--- test/controllers/api/v1/cars_controller_test.rb
+++ test/controllers/api/v1/cars_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class Api::V1::CarsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save car without car_type" do
+    car = Car.new
+    assert_not car.save
+  end
+
+  test "should save car with details" do
+    car = Car.new(:car_type => 'electric', :car_model => 'tesla', :car_variant => 'S', :car_year => 2019)
+    assert car.save
+  end
+
+  test "should save car with details in the database" do
+    car = Car.new(:car_type => 'electric', :car_model => 'tesla', :car_variant => 'S', :car_year => 2019)
+    car.save
+    car_db =  Car.where(:car_type => 'electric').first()
+    assert_equal(car[:car_model], car_db[:car_model])
+  end
 end
